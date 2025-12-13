05a8e8e81633a59b0f1e8a7b6487559b2839a637
JoyOrji <ninaorji5@gmail.com>
2025-12-13 10:00:59 +0100

    Total workout added

--- fitness_tracker_app/src/App.jsx
+++ fitness_tracker_app/src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 function App() {
+  const [workouts, setWorkouts] = useState([]);
+
   return (
     <div>
       <h1>Fitness Tracker 💪</h1>
+      <p>Total workouts: {workouts.length}</p>
     </div>
   );
 }
