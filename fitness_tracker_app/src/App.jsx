695e675e25ece9e7a8d66c01a58255294a77c3ab
JoyOrji <ninaorji5@gmail.com>
2025-12-22 22:32:45 +0100

    Add changes recently made

--- fitness_tracker_app/src/App.jsx
+++ fitness_tracker_app/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-purple-400 via-pink-400 to-yellow-400 flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">
+      <h1 className="text-orange text-4xl font-bold">
         Tailwind is working!
       </h1>
     </div>
