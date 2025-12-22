8e6d0a07ff70c63089ab2311d0fec172199d62c0
JoyOrji <ninaorji5@gmail.com>
2025-12-22 23:12:48 +0100

    Add changes recently made

--- fitness_tracker_app/src/App.jsx
+++ fitness_tracker_app/src/App.jsx
@@ -1,28 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import SignIn from "./pages/SignIn";
-import Gender from "./pages/Gender";
-import Age from "./pages/Age";
-import Weight from "./pages/Weight";
-import Dashboard from "./pages/Dashboard";
-import WorkoutPlan from "./pages/WorkoutPlan";
-import Profile from "./pages/Profile";
-
-export default function App() {
+ export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-200 via-pink-200 to-yellow-200">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/gender" element={<Gender />} />
-          <Route path="/age" element={<Age />} />
-          <Route path="/weight" element={<Weight />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/workout-plan" element={<WorkoutPlan />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 flex items-center justify-center">
+      <h1 className="text-white text-5xl font-extrabold">
+        Tailwind is FINALLY working 🎉
+      </h1>
     </div>
   );
-}
+} 
