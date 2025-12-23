20015351f68bd19622644d6cd40bbec6de5141b8
JoyOrji <ninaorji5@gmail.com>
2025-12-23 06:04:19 +0100

    Add changes recently made

--- fitness_tracker_app/src/App.jsx
+++ fitness_tracker_app/src/App.jsx
@@ -23,29 +23,4 @@ export default function App() {
       </Routes>
     </BrowserRouter>
   );
-}import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import SignIn from "./pages/SignIn";
-import Gender from "./pages/Gender";
-import Age from "./pages/Age";
-import Weight from "./pages/Weight";
-import Dashboard from "./pages/Dashboard";
-import WorkoutPlan from "./pages/WorkoutPlan";
-import Profile from "./pages/Profile";
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/gender" element={<Gender />} />
-        <Route path="/age" element={<Age />} />
-        <Route path="/weight" element={<Weight />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/workout-plan" element={<WorkoutPlan />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
