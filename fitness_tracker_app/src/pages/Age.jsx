19be229cf2f32873db08e827684c682b42abb67e
JoyOrji <ninaorji5@gmail.com>
2025-12-23 10:48:40 +0100

    Add changes recently made

--- fitness_tracker_app/src/pages/Age.jsx
+++ fitness_tracker_app/src/pages/Age.jsx
@@ -5,7 +5,7 @@ export default function Age() {
   const navigate = useNavigate();
   const [selected, setSelected] = useState(34);
 
-  const ages = [31, 32, 33, 34, 35, 36, 37];
+  const ages = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
 
   return (
     <div className="min-h-screen bg-[#0D0D0D] text-white flex flex-col p-6">
